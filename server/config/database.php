701fdab2d2a4ee3bc40cfbceef9c1bb95911deed
verse91 <versedev.store@proton.me>
2026-04-07 20:34:27 +0700

    fix(config): follow .env variables' name standard

--- server/config/database.php
+++ server/config/database.php
@@ -1,14 +1,13 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "toeic_online";
+$servername = getenv('DB_HOST') ?: "db";
+$username = getenv('MYSQL_USER') ?: "root";
+$password = getenv('MYSQL_PASSWORD') ?: "";
+$dbname = getenv('MYSQL_DATABASE') ?: "prephub";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
-	die("Connection failed: " . $conn->connect_error);
+  die("connection failed: " . $conn->connect_error);
 }
-?>
