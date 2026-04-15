d634f29c3134f80e5ae00998bc2ef45e3f003554
Nhan-1234 <24521234@gm.uit.edu.vn>
2026-04-15 18:28:05 +0700

    fix(attempts,dashboards)

--- client/pages/dashboard.php
+++ client/pages/dashboard.php
@@ -1,8 +1,7 @@
 <!DOCTYPE html>
 <html lang="vi">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php include './components/metadata.php'; ?>
     <title>TOEIC Dashboard </title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="..\styles\dashboardStyle.css">
