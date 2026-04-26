576a3316041730f0a20eff9791849fb4e4a7ee16
Hoang Huy <hle9971@gmail.com>
2026-04-26 15:47:18 +0700

    feat(): thêm cổng api cho dashboard và score

--- server/routes/api.php
+++ server/routes/api.php
@@ -25,6 +25,14 @@ switch ($resource) {
 		require_once __DIR__ . '/passages.php';
 		break;
 
+	case 'dashboard':
+        require_once __DIR__ . '/dashboard.php';
+        break;
+
+	case 'score':
+         require_once __DIR__ . '/../controllers/score-controller.php';
+        break;
+
 	default:
 		http_response_code(404);
 		echo json_encode([
