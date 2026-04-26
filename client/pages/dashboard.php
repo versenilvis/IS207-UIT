611f2622181380ed0e8ea1bf5da0ae690434f111
Hoang Huy <hle9971@gmail.com>
2026-04-26 15:44:02 +0700

    đổi src từ file data_sample sang trang dashboard

--- client/pages/dashboard.php
+++ client/pages/dashboard.php
@@ -81,6 +81,6 @@
 </div>
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-<script src="..\js\data_sample.js"></script>
+<script src="../js/dashboard.js"></script>
 </body>
 </html>
