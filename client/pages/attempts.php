582b59b6b949516f0fae0cd923c8a6d7a2a9edcd
Hoang Huy <hle9971@gmail.com>
2026-05-17 12:45:54 +0700

    feat(): sử dụng các metadata.php

--- client/pages/attempts.php
+++ client/pages/attempts.php
@@ -4,8 +4,6 @@
 <head>
     <?php include './components/metadata.php'; ?>
     <title>Lịch sử làm bài - TOEIC Project</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <link rel="stylesheet" href="../styles/attempts.css">
 </head>
 
